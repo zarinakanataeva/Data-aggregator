a65d82519be75f1b069da802b298720f7546e598
Zarina Kanataeva <kanataevazarina@icloud.com>
2026-01-15 13:13:41 +0800

    added classes for different types of transaction

--- Fin_tracker/src/main/java/com/skillbox/data/model/Account.java
+++ Fin_tracker/src/main/java/com/skillbox/data/model/Account.java
@@ -1,10 +1,42 @@
 package com.skillbox.data.model;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * Класс представляющий собой счет в банке
  */
 // TODO: Исправьте этот класс, он не должен быть абстрактным
-public abstract class Account implements AccountInfo, BalanceOperations, AccountStatement {
+public class Account implements AccountInfo, BalanceOperations, AccountStatement {
 
 
+    @Override
+    public int getAccountId() {
+        return 0;
+    }
+
+    @Override
+    public int getUserId() {
+        return 0;
+    }
+
+    @Override
+    public AccountType getAccountType() {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> getTransactions() {
+        return List.of();
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return null;
+    }
+
+    @Override
+    public void addTransaction(Transaction transaction) {
+
+    }
 }
